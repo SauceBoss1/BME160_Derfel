aecac87afadf7d2569d72ef4defc3fb17b81d869
DerfelT <dtvsworld@gmail.com>
2021-04-05 13:10:53 -0700

    Lab 02

---
+++ Lab 2/fastqParse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3 
+# Name: Your full name 
+# Group Members: List full names or “None”
+
+'''
+Program docstring goes here.
+'''
+
+class FastqString (str):
+    ''' Class docstring goes here.'''
+    def parse(self):
+        ''' Method docstring goes here.'''
+        pass
+def main():
+    ''' Function docstring goes here.'''
+    pass
+
+main()
