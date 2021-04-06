e85deaa2ad69642c1d6ed59b0c90665b39e6a2eb
DerfelT <dtvsworld@gmail.com>
2021-04-05 18:16:21 -0700

    Lab 2

--- Lab 2/fastqParse.py
+++ Lab 2/fastqParse.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3 
-# Name: Your full name 
-# Group Members: List full names or “None”
+# Name: Derfel Terciano (dtercian)
+# Group Members: None
+'''Sequence information parsing
+In this exercise, you will create a program to “parse” sequence name information from a single line of a FASTQ formatted file. Your task is to create a Python script called fastqParse that:
+
+asks for and collects the seqname line of a FASTQ file using input()
+parses out each field of the run information from the string and displays each of them on a new line For example, if I enter the FASTQ seqname line:
+@EAS139:136:FC706VJ:2:2104:15343:197393 then the program will output:
+Instrument = EAS139
+Run ID = 136
+Flow Cell ID = FC706VJ
+Flow Cell Lane = 2
+Tile Number = 2104
+X-coord = 15343
+Y-coord = 197393
+
+Hints:
+The input string is guaranteed to have 7 fields.
+The first character of the FASTQ seqname line is “@” and each field of the run information is separated by a colon“:”.
+A reasonable solution would be around 16 lines of code excluding comments.
+To get full credit on this assignment, your code needs to:
+
+Run properly (execute and produce the correct output)
+Contain documentation/comments
+Include any assumptions or design decisions you made in writing your code
+Include an overview describing what your program does with expected inputs and outputs
+'''
 
 '''
 Program docstring goes here.
@@ -10,9 +35,22 @@ class FastqString (str):
     ''' Class docstring goes here.'''
     def parse(self):
         ''' Method docstring goes here.'''
-        pass
+        qString = self
+        qString = qString.replace('@','')
+        qString = qString.split(':')
+
+
+        instrument = 'Intrument = '+ f'{qString[0]}' +'\n'
+        runID = 'Run ID = ' + f'{qString[1]}' +'\n'
+        flowCellID = 'Flow Cell ID = ' + f'{qString[2]}' +'\n'
+        flowCellLane = 'Flow Cell Lane = ' + f'{qString[3]}' +'\n'
+        tileNumber = 'Tile Number = ' + f'{qString[4]}' +'\n'
+        xCoord = 'X-coord = ' + f'{qString[5]}' +'\n'
+        yCoord = 'Y-coord = ' + f'{qString[6]}'
+        return instrument + runID + flowCellID + flowCellLane + tileNumber + xCoord + yCoord
 def main():
     ''' Function docstring goes here.'''
-    pass
+    fastQ = FastqString(input('FASTQ Seq name: '))
+    print(fastQ.parse())
 
 main()
