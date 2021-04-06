d13db09c4a1580417e72b134d71201892c7c678f
DerfelT <dtvsworld@gmail.com>
2021-04-06 16:34:44 -0700

    Lab 2

---
+++ Lab 2/converter.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3 
+# Name: Derfel Terciano (dtercian) 
+# Group Members: None
+
+'''
+Program docstring goes here
+'''
+short_AA = {
+            'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+            'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
+            'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+            'ALA': 'A', 'VAL': 'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'
+            }
+
+long_AA = {value:key for key,value in short_AA.items()}
+
+RNA_codon_table = {
+# Second Base
+# U             C             A             G
+#U
+'UUU': 'Phe', 'UCU': 'Ser', 'UAU': 'Tyr', 'UGU': 'Cys',
+'UUC': 'Phe', 'UCC': 'Ser', 'UAC': 'Tyr', 'UGC': 'Cys',
+'UUA': 'Leu', 'UCA': 'Ser', 'UAA': '---', 'UGA': '---',
+'UUG': 'Leu', 'UCG': 'Ser', 'UAG': '---', 'UGG': 'Trp',
+#C 
+'CUU': 'Leu', 'CCU': 'Pro', 'CAU': 'His', 'CGU': 'Arg',
+'CUC': 'Leu', 'CCC': 'Pro', 'CAC': 'His', 'CGC': 'Arg',
+'CUA': 'Leu', 'CCA': 'Pro', 'CAA': 'Gln', 'CGA': 'Arg',
+'CUG': 'Leu', 'CCG': 'Pro', 'CAG': 'Gln', 'CGG': 'Arg',
+#A
+'AUU': 'Ile', 'ACU': 'Thr', 'AAU': 'Asn', 'AGU': 'Ser',
+'AUC': 'Ile', 'ACC': 'Thr', 'AAC': 'Asn', 'AGC': 'Ser',
+'AUA': 'Ile', 'ACA': 'Thr', 'AAA': 'Lys', 'AGA': 'Arg',
+'AUG': 'Met', 'ACG': 'Thr', 'AAG': 'Lys', 'AGG': 'Arg',
+#G
+'GUU': 'Val', 'GCU': 'Ala', 'GAU': 'Asp', 'GGU': 'Gly',
+'GUC': 'Val', 'GCC': 'Ala', 'GAC': 'Asp', 'GGC': 'Gly',
+'GUA': 'Val', 'GCA': 'Ala', 'GAA': 'Glu', 'GGA': 'Gly',
+'GUG': 'Val', 'GCG': 'Ala', 'GAG': 'Glu', 'GGG': 'Gly'
+}
+dnaCodonTable = {key.replace('U','T'):value for key, value in RNA_codon_table.items()}
+
+def nucInterpreter (inputString):
+    '''This function determins the correct dictionary output based on what input is given'''
+    inputString = inputString.upper() #Completely unnecessary but just in case a lowercased input is given
+    if inputString.isupper() & ('T' in inputString): #DNA codon
+        return f'{inputString} = ' + f'{dnaCodonTable.get(inputString,"unknown").upper()}'
+    elif inputString.isupper() & ('U' in inputString): #RNA codon
+        return f'{inputString} = ' + f'{RNA_codon_table.get(inputString,"unknown").upper()}'
+    elif len(inputString)==3: #Short AA
+        return f'{inputString} = ' + f'{short_AA.get(inputString,"unknown")}'
+    elif len(inputString)==1: #long AA
+        return f'{inputString} = ' + f'{long_AA.get(inputString,"unknown")}'
+    else:
+        return 'unknown'
+
+def main():
+    ''' Function docstring goes here'''
+    print(nucInterpreter(input('Enter String: ')))
+
+main()
