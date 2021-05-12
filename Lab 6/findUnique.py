06f0b50b18d5f241ccd933bfa587812d00ec3ddf
DerfelT <dtvsworld@gmail.com>
2021-05-11 23:48:05 -0700

    Lab 6

--- Lab 6/findUnique.py
+++ Lab 6/findUnique.py
@@ -1,34 +1,74 @@
 #!/usr/bin/env python3
-# Name: Your full name (CATS account username)
+# Name: Derfel Terciano (dtercian)
 # Group Members: List full names (CATS usernames) or “None”
+# Design for findUnique
 
+# 1) input all sequences into class
+# 2) find power set for all of the tRNA sequences and store it into the class
+# 3) have a function that takes in a header, and do all the set comparison based on the header and sequence
+# 4) have the final output be created based on calling the header of each sequence           
 import sys
 from sequenceAnalysis import FastAreader #type: ignore
-
 class FindUnique:
-    def __init__(self, seq=''):
-        self.tRnaInfo ={}
-        self.addSeq(seq)
+    def __init__(self, header='', seq=''):
+        self.tRnaInfo ={} #the form of the dictionary is in the following => header: [seq, powerset(seq)] 
+        self.headers = []
+        self.addSeq(seq, header)    
 
     def addSeq(self, seq, header):
-        seq= seq.upper().replace('-','').replace('_','').replace('.','')
-        self.tRnaInfo[header] = [seq, self.powerset(self.seq)]
-        
+        header = header.replace(' ','')
+        self.seq= seq.upper().replace('-','').replace('_','').replace('.','')
+        if header != '':
+            self.headers.append(header)
+            self.tRnaInfo[header] = [self.seq, self.powerset(self.seq)]
+
     def powerset(self,inSeq):
         pSet= set()
         for i in range(len(inSeq)):
             for j in range(i+1,len(inSeq)+1):
                 pSet.add(inSeq[i:j])
         return pSet
-'''
-Design for findUnique
 
-1) input all sequences into class
-2) find power set for all of the tRNA sequences and store it into the class
-3) have a function that takes in a header, and do all the set comparison based on the header and sequence
-4) have the final output be created based on calling the header of each sequence
-'''
-            
+    def uniqueFinder(self, curHeader):
+        allSets = set()
+        for header, seq in self.tRnaInfo.items():
+            if curHeader != header:
+                allSets = allSets | seq[1]
+        return set(self.tRnaInfo[curHeader][1]) - allSets
+
+    def essential(self, curHeader):
+        nonEssentials = []
+        uniqueSet = list(self.uniqueFinder(curHeader))
+        compareSet = uniqueSet.copy()
+        for subset in uniqueSet:
+            compareSet.remove(subset)
+            for compareSub in compareSet:
+                if compareSub in subset:
+                    nonEssentials.append(subset)
+            compareSet.append(subset)
+        return set(uniqueSet) - set(nonEssentials)
+
+    def findAllOccurances(self, subSeq, mainSeq):
+        posFound = []
+        index = mainSeq.find(subSeq)
+        while index != -1:
+            posFound.append(index)
+            index = mainSeq.find(subSeq, index+1)
+        return posFound
+    
+    def findUnique(self): #problem: some essentials have multiple positions on sequence
+        sys.stdout.reconfigure(encoding='utf8')
+        with sys.stdout as outFile:
+            for currentHeader in sorted(self.headers, key = lambda a:a[5:8]): #adjust order of sequences here
+                print(f'{currentHeader}\n{self.tRnaInfo[currentHeader][0]}')
+                essentials = self.essential(currentHeader)
+                essentialsIndex = {}
+                for essentialSubs in essentials:
+                    for indexes in self.findAllOccurances(essentialSubs, self.tRnaInfo[currentHeader][0]):
+                        essentialsIndex[indexes] = essentialSubs
+                for index, seq in sorted(essentialsIndex.items(), key = lambda a:a[0]):
+                    print(f'{index * "."}{seq}')
+
 ########################################################################
 # Main
 # Here is the main program
@@ -36,7 +76,10 @@ Design for findUnique
 ########################################################################
 
 def main(inCL=None):
-    ''' '''
-    pass
+    inFile = FastAreader()
+    genes = FindUnique()
+    for header,seq in inFile.readFasta():
+        genes.addSeq(seq, header)
+    genes.findUnique()
 if __name__ == "__main__":
     main()  
--- Lab 6/testSpace.py
+++ Lab 6/testSpace.py
@@ -6,7 +6,50 @@ from findUnique import FindUnique #type: ignore
 curSet = {'ATG','GTG','NNN'}
 
 print(curSet-{'ATG','GTG'})
-
+genes = {'gene1': ['ATGGCG', {'ATG', 'GCG', 'gg','g', 'cc','c'}], 'gene2':['GGG',{'gg','g'}], 'gene3':['ccc',{'cc','c'}]}
 un1 = set()
 un2 = {'1','2','3'}
 print(un1 - un2)
+
+#print(genes['gene1'][1])
+
+def uniqueFinder(curHeader):
+    allSets = []
+    for header, seq in genes.items():
+        if curHeader != header:     
+            allSets.append(genes[header][1])
+    return genes[curHeader][1] - set().union(*allSets)
+
+print('final: ',uniqueFinder('gene1'))
+
+set1 = {'AAAACG','ACG'}
+
+def essential(inSet):
+    nonEssential = []
+    uniqueSet = list(inSet)
+    compareSet = list(uniqueSet.copy())
+    for subset in uniqueSet:
+        compareSet.remove(subset)
+        for compareSub in compareSet:
+            if compareSub in subset:
+                nonEssential.append(subset)
+                print('nonEssential: ', subset)
+        compareSet.append(subset)
+    return set(uniqueSet) - set(nonEssential)
+
+print('essential', essential(set1))
+
+seq = 'CCCACTGACTGACTGGGACTGGTT'
+print(seq.find('GGG'))
+
+test11 = { 0: 'aaa', 1: '2222'}
+print(test11[0])
+
+def findAllOccurances( subSeq, mainSeq):
+    posFound = []
+    i = mainSeq.find(subSeq)
+    while i != -1:
+        posFound.append(i)
+        i = mainSeq.find(subSeq, i+1)
+    return posFound
+print(str(findAllOccurances('AAAAUA', 'UGGUACUU"GUUUAAAAUAAAAUAAAUGAUUUCGACPCAUUAGAUUAUGAUUUAAUUCAUAAUUACCAACCA')))
