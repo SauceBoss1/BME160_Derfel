cf977df994dc9eff57dc1485d6e58ead582d4edc
DerfelT <dtvsworld@gmail.com>
2021-06-06 23:22:40 -0700

    fastq

--- FastQtranslator/commandLine.py
+++ FastQtranslator/commandLine.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python3
 # Collaborators: Sagarika Kannoly (skannoly) & Derfel Terciano (dtercian)
+'''
+Handle all command line options for the fastQ translator
 
+Written by: Derfel Terciano & Sagarika Kannoly
+
+Contains:
+    -CommandLine() [object] - an object that handles command line options
+'''
 class CommandLine():
+    '''
+    Handle all command line arguments for fastQ translation
+
+    Written By: Derfel Terciano & Sagarika Kannoly
+
+    Options:
+        Input handling:
+            -args.PHRED33input
+            -args.PHRED64input
+            -args.PHRED64Binput
+            -args.PHRED64SOLinput
+        Output Handling:
+            -args.PHRED64output
+            -args.PHRED33output
+    
+    For usage use the -h flag in the command line
+    '''
 
     def __init__(self, inOpts = None):
+        '''
+        Usage: 
+            1) instantiate class
+            2) use .args.argument as an input in your program
+        '''
         import argparse
         self.parser = argparse.ArgumentParser(description = 'handles the fastQ command input',
                                                 add_help= True,
@@ -11,15 +40,15 @@ class CommandLine():
                                                 usage = '%(prog)s [options] -option1[default] <input >output'
                                                 )
 
-        self.parser.add_argument('-P33in', '--PHRED33input', action = 'store', nargs='?', const=True, default=False, help = 'PHRED33 input')
-        self.parser.add_argument('-P64in','--PHRED64input', action = 'store', nargs= '?', const=True, default=False, help = 'PHRED64 input')
-        self.parser.add_argument('-P64Bin','--PHRED64Binput', action= 'store', nargs='?',const=True, default=False, help= 'PHRED64 with B offset in quality values')
-        self.parser.add_argument('-P64SOLin', '--PHRED64SOLinput', action='store',nargs='?',const='True',default=False, help = 'PHRED64 with SOLEXA interpretation of Q score')
+        self.parser.add_argument('-P33in', '--PHRED33input', action = 'store', nargs='?', const=True, default=False, help = 'PHRED33 input') #PHRED 33 input specifier/argument
+        self.parser.add_argument('-P64in','--PHRED64input', action = 'store', nargs= '?', const=True, default=False, help = 'PHRED64 input') #PHRED 64 input specifier/argument
+        self.parser.add_argument('-P64Bin','--PHRED64Binput', action= 'store', nargs='?',const=True, default=False, help= 'PHRED64 with B offset in quality values') #PHRED 64 B Offset input specifier/argument
+        self.parser.add_argument('-P64SOLin', '--PHRED64SOLinput', action='store',nargs='?',const='True',default=False, help = 'PHRED64 with SOLEXA interpretation of Q score') #PHRED 64 Solexa Scoring input specifier/argument
 
-        self.parser.add_argument('-P33out', '--PHRED33output', action='store',nargs='?',const='True',default=False, help = 'output to PHRED33')
-        self.parser.add_argument('-P64out', '--PHRED64output', action='store',nargs='?',const='True',default=False, help = 'output to PHRED64')
+        self.parser.add_argument('-P33out', '--PHRED33output', action='store',nargs='?',const='True',default=False, help = 'output to PHRED33') #PHRED 33 output specifier/argument
+        self.parser.add_argument('-P64out', '--PHRED64output', action='store',nargs='?',const='True',default=False, help = 'output to PHRED64') #PHRED 33 output specifier/argument
 
-        if inOpts is None:
-            self.args = self.parser.parse_args()
+        if inOpts is None: #handles command line input from stdin
+            self.args = self.parser.parse_args() 
         else:
-            self.args = self.parser.parse_args(inOpts)
+            self.args = self.parser.parse_args(inOpts) #parse only specific commands if specified
--- FastQtranslator/conversions.py
+++ FastQtranslator/conversions.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # Collaborators: Sagarika Kannoly (skannoly) & Derfel Terciano (dtercian)
-'''Functions for all phred conversions'''
+'''
+Convert PHRED scores to different PHRED formats
+
+Written By: Derfel Terciano & Sagarika Kannoly
+
+Summary: 
+    -take in a quality line, sequence, and return the newly converted PHRED quality line from the specified output
+
+Objects:
+    -PhredConversions (qScore, seq, clOut = False) -> converts all quality scores to a spcifed PHRED format
+
+Notes:
+    -Solexa conversions run in around 3 seconds
+    -All other conversions run in 0.3 seconds
+'''
 
 # ########################################
 # HOW TO APPROACH THE PROJECT
@@ -20,12 +34,28 @@ from solexaDictionary import solexaToPhred, asciiToSolexa  # remember to have th
 
 
 class PhredConversions:
+    '''
+    Convert all quality scores to a specifed PHRED format
+
+    Written by: Derfel Terciano & Sagarika Kannoly
+
+    Methods:
+        -getNewSeq() -> returns the converted sequence (is converted at all)
+        -outputConverter() -> returns the PHRED33 or PHRED64 offset
+        -p33toPhred() -> converts PHRED33 to either phred 33 or phred 64
+        -p64toPhred() -> converts PHRED64 to either phred 33 or phred 64
+        -p64bToPhred() -> converts PHRED64 with B offset to either phred 33 or phred 64
+        -pSolToPhred() -> converts PHRED64 with Solexa scoring to either phred 33 or phred 64
+
+    '''
     def __init__(self, qScores, seq, clOut=False):
-        self.clOut = clOut
+        '''Initialize object with a required quality line, sequence, and an optional output of either p33[default] or p64 format'''
+        self.clOut = clOut #True -> use p64 format // False -> use p33 format
         self.qScore = qScores
         self.newSeq = seq  # this is only needed if we use B offset
 
     def getNewSeq(self):
+        '''return the sequence'''
         return self.newSeq
 
     def outputConverter(self, clOut):
@@ -38,17 +68,17 @@ class PhredConversions:
     def p33toPhred(self):
         '''convert p33 to qScores that range from (0:40)'''
         phredQscore = ''
-        for char in self.qScore:
-            rawQscore = ord(char) - 33
-            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut)))
+        for char in self.qScore: #iterate through each quality character
+            rawQscore = ord(char) - 33 #find its quality score in the (0:40) range
+            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut))) #covert each rawQscore to either p33 or p64 and then find its ascii char
         return phredQscore
 
     def p64toPhred(self):
         '''convert p64 to qScores that range from (0:40)'''
         phredQscore = ''
-        for char in self.qScore:
-            rawQscore = ord(char) - 64
-            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut)))
+        for char in self.qScore: #iterate through each quality character
+            rawQscore = ord(char) - 64 #find its quality score in the (0:40) range
+            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut))) #covert each rawQscore to either p33 or p64 and then find its ascii char
         return phredQscore
 
     def p64bToPhred(self):
@@ -56,23 +86,23 @@ class PhredConversions:
         newSeq = self.newSeq
         phredQscore = ''
         charPos = 0
-        for char in self.qScore:
-            if ord(char) == 66:
+        for char in self.qScore: #iterate through each quality character
+            if ord(char) == 66: #if we see 'B' then it's considered the lowest score so assign it to 0
                 rawQscore = 0
             else:
-                rawQscore = ord(char) - 64
+                rawQscore = ord(char) - 64 #else find its qScore in the (0:40) range
 
-            if rawQscore == 0:
+            if rawQscore == 0: #when we find 'B' or the lowest qScore, change it's corresponding base position in the seq to N
                 newSeq = newSeq[:charPos] + 'N' + newSeq[charPos + 1:]
-            charPos += 1
-            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut)))
+            charPos += 1 #increase the character position
+            phredQscore += phredQscore.join(chr(rawQscore + self.outputConverter(self.clOut))) #covert each rawQscore to either p33 or p64 and then find its ascii char
         return phredQscore, newSeq
 
     def pSolToPhred(self):
         '''convert p64Solexa offset to qScores that range from (0:40)'''
         phredQscore = ''
         for char in self.qScore:
-            solQ = ord(char)
-            rawQ = solexaToPhred[asciiToSolexa[solQ]]
-            phredQscore += phredQscore.join(chr(rawQ + self.outputConverter(self.clOut)))
+            solQ = ord(char) #find the ascii integer of the current character
+            rawQ = solexaToPhred[asciiToSolexa[solQ]] #using dictionary mappings, find the qScore in the (0:40) range
+            phredQscore += phredQscore.join(chr(rawQ + self.outputConverter(self.clOut))) #covert each rawQscore to either p33 or p64 and then find its ascii char
         return phredQscore
